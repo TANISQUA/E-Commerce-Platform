21db9cbc4937140a98e35cd66799d72b859042ed
TANISQUA <guhatanisqua@gmail.com>
2025-08-10 21:03:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active'); 
+
+});
